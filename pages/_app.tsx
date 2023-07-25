b6ef363d68c3e75489d0ea31e4cef027c768f368
Matthew WIlkinson <matt@zenshop.app>
2023-07-25 13:09:10 +0100

    use script in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,33 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      disableTransitionOnChange
-      defaultTheme="system"
-      attribute="class"
-    >
-      <a
-        href="#main"
-        className="fixed p-2 top-0 left-0 -translate-y-full focus:translate-y-0"
+    <>
+      <Script async src="https://helpdesk.zenshop.app/bundle.js?helpdeskId=kHyvy8MMAXxqFDfdED85AgEds-PYvGZZozh062MeyFO5VR0H5A==" />
+      <ThemeProvider
+        disableTransitionOnChange
+        defaultTheme="system"
+        attribute="class"
       >
-        Skip to main content
-      </a>
-      <div className="flex flex-col max-w-3xl mx-auto min-h-full px-4">
-        <Header />
-        <main id="main">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-    </ThemeProvider>
+        <a
+          href="#main"
+          className="fixed p-2 top-0 left-0 -translate-y-full focus:translate-y-0"
+        >
+          Skip to main content
+        </a>
+        <div className="flex flex-col max-w-3xl mx-auto min-h-full px-4">
+          <Header />
+          <main id="main">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </>
+
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Document, { DocumentContext } from "next/document";
 import { cx } from "@/lib/utils";
-import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -22,7 +21,6 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-        <Script async src="https://helpdesk.zenshop.app/bundle.js?helpdeskId=kHyvy8MMAXxqFDfdED85AgEds-PYvGZZozh062MeyFO5VR0H5A==" />
       </Html>
     );
   }
